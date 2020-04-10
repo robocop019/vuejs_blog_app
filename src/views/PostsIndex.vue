643fd79ee48e8b1c7e7cb102118843faf71bcb24
robocop019 <chrisbowen@comcast.net>
2020-04-09 19:25:25 -0500

    initial commit

---
+++ src/views/PostsIndex.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class='posts-index'>
+    <h1>All Blog Posts</h1>
+
+    <div v-for='post in posts'>
+      <h3> {{post.title}} </h3>
+      <img :src="post.image" :alt="post.title">
+      <p> {{post.body}} </p>
+    </div> <!-- end v for loop -->
+
+  </div>
+</template>
+
+<style>
+  
+</style>
+
+<script>
+  var axios = require('axios');
+
+  export default {
+    data: function() {
+      return {
+        posts: []
+      };
+    },
+    created: function() {
+      axios.get('/api/posts').then(response => {
+        console.log(response.data);
+        this.posts = response.data;
+      });
+    },
+    methods: {}
+  };
+</script>
